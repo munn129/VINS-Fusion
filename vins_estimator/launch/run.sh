99e43afc160c99b52203537d8ab3f44daa1ebb3f
vins_fusion <ans92175@gmail.com>
2024-07-25 06:17:44 +0000

    clear unuse codes

--- vins_estimator/launch/run.sh
+++ vins_estimator/launch/run.sh
@@ -31,9 +31,7 @@ echo "BAG DURATION:" $BAG_DUR "=> LOG_DUR:" $LOG_DUR;
 
 let ANC_MAX=ANC_ID_MAX+1
 
-roslaunch vins vio.launch log_dir:=$EXP_OUTPUT_DIR \
-log_dir:=$VIRAL_OUTPUT_DIR \
-autorun:=true \
+roslaunch vins vio.launch \
 bag_file:=$DATASET_LOCATION/$EXP_NAME/$EXP_NAME.bag 
 
 wait;
