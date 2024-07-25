d29dea99c945fcf4aed46026189954ad9fc89395
vins_fusion <ans92175@gmail.com>
2024-07-25 06:51:18 +0000

    real time settings complate

--- vins_estimator/launch/run.sh
+++ vins_estimator/launch/run.sh
@@ -5,33 +5,8 @@ export USER=workspace
 catkin_make -C /workspace/catkin_ws ;
 source /workspace/catkin_ws/devel/setup.bash ;
 
-# Get the current directory
-CURR_DIR=$(pwd)
-# Get the location of the viral package
-roscd vins
-PACKAGE_DIR=$(pwd)
-# Return to the current dir, print the directions
-cd $CURR_DIR
-echo CURRENT DIR: $CURR_DIR
-echo VINS DIR:    $PACKAGE_DIR
-
-export DATASET_LOCATION=/workspace/NTU_VIRAL_DATASET
-
 wait;
 
-export EXP_NAME=eee_01;
-export LOG_DUR=450;
-export FUSE_VIS=1;
-export ANC_ID_MAX=-1;
-
-export BAG_DUR=$(rosbag info $DATASET_LOCATION/$EXP_NAME/$EXP_NAME.bag | grep 'duration' | sed 's/^.*(//' | sed 's/s)//');
-let LOG_DUR=BAG_DUR+20
-
-echo "BAG DURATION:" $BAG_DUR "=> LOG_DUR:" $LOG_DUR;
-
-let ANC_MAX=ANC_ID_MAX+1
-
-roslaunch vins vio.launch \
-bag_file:=$DATASET_LOCATION/$EXP_NAME/$EXP_NAME.bag 
+roslaunch vins vio.launch 
 
 wait;
